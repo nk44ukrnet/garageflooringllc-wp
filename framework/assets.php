f9168b8496bd263b7562de90b1d6da98e482ab26
nk44ukrnet <56731417+nk44ukrnet@users.noreply.github.com>
2023-03-28 14:30:25 +0300

    search results additional CSS JS from assets added

--- framework/assets.php
+++ framework/assets.php
@@ -5,6 +5,8 @@ function theme_files()
 
     wp_register_script('swiper-js', get_theme_file_uri('/assets/script/vendor/swiper-bundle.min.js'), NULL, '1', false);
     wp_register_script('slinky-js', get_theme_file_uri('/assets/script/vendor/slinky.min.js'), NULL, '1', false);
+    wp_register_script('add-search-plugin-js-url', 'https://cdn.jsdelivr.net/combine/npm/addsearch-js-client@0.6/dist/addsearch-js-client.min.js,npm/addsearch-search-ui@0.7/dist/addsearch-search-ui.min.js', NULL, '1', false);
+    wp_register_script('add-search-plugin-js-local', get_theme_file_uri('/assets/script/vendor/addsearch-plugin-ui.min.js'), NULL, '1', true);
     wp_register_script('main-js', get_theme_file_uri('/assets/script/main.js'), NULL, filemtime(get_template_directory() . '/assets/script/main.js'), true);
 
     wp_register_style('normalize', get_template_directory_uri() . '/assets/styles/vendor/normalize.min.css', NULL, '1', 'all');
@@ -12,19 +14,25 @@ function theme_files()
     wp_register_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Rubik:wght@400;500;600;700;900&display=swap', NULL, '1', 'all');
     wp_register_style('swiper-style', get_template_directory_uri() . '/assets/styles/vendor/swiper-bundle.css', NULL, '1', 'all');
     wp_register_style('slinky-styles', get_template_directory_uri() . '/assets/styles/vendor/slinky.min.css', NULL, '1', 'all');
+    wp_register_style('add-search-plugin-css-url', 'https://cdn.jsdelivr.net/npm/addsearch-search-ui@0.7/dist/addsearch-search-ui.min.css', NULL, '1', 'all');
+    wp_register_style('add-search-plugin-css-local', get_template_directory_uri() . '/assets/styles/vendor/addsearch-plugin-ui.min.css', NULL, '1', 'all');
     wp_register_style('main-styles', get_template_directory_uri() . '/assets/styles/all.css', NULL, filemtime(get_template_directory() . '/assets/styles/all.css'), 'all');
 
-    //scripts
+    //enqueue scripts
     wp_enqueue_script('swiper-js');
     wp_enqueue_script('slinky-js');
+    wp_enqueue_script('add-search-plugin-js-url');
+    wp_enqueue_script('add-search-plugin-js-local');
     wp_enqueue_script('main-js');
 
-    //styles
+    //enqueue styles
     wp_enqueue_style('normalize');
     wp_enqueue_style('font-awesome');
     wp_enqueue_style('swiper-style');
     wp_enqueue_style('google-fonts');
     wp_enqueue_style('slinky-styles');
+    wp_enqueue_style('add-search-plugin-css-url');
+    wp_enqueue_style('add-search-plugin-css-local');
     wp_enqueue_style('main-styles');
 
 
